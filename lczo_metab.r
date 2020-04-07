d160b743500875749b48e3429f36ec9ac4816baa
kzollove <kylezollo@gmail.com>
2020-04-07 13:53:31 -0400

    added discharge

--- lczo_metab.r
+++ lczo_metab.r
@@ -1,18 +1,6 @@
-list_of_packages <- c("remotes")
-new_packages <- list_of_packages[!(list_of_packages %in% installed.packages()[,"Package"])]
-if(length(new_packages)) install.packages(new_packages, dependencies = TRUE)
-
-list_of_appling_packages <- c("unitted")
-new_appling_packages <- list_of_appling_packages[!(list_of_appling_packages %in% installed.packages()[,"Package"])]
-new_appling_packages <- paste("appling/", new_appling_packages, sep="")
-if(length(new_appling_packages)) remotes::install_github(new_appling_packages, INSTALL_opts = '--no-lock')
-#Based off of: remotes::install_github('appling/unitted', INSTALL_opts = '--no-lock') 
-
-list_of_usgs_packages <- c("streamMetabolizer")
-new_usgs_packages <- list_of_usgs_packages[!(list_of_usgs_packages %in% installed.packages()[,"Package"])]
-new_usgs_packages <-  paste("USGS-R/", new_usgs_packages, sep="")
-if(length(new_usgs_packages)) remotes::install_github(new_usgs_packages, INSTALL_opts = '--no-lock')
-#Based off of: remotes::install_github("USGS-R/streamMetabolizer", INSTALL_opts = '--no-lock')
+#install.packages(remotes)
+#remotes::install_github('appling/unitted', INSTALL_opts = '--no-lock') 
+#remotes::install_github("USGS-R/streamMetabolizer", INSTALL_opts = '--no-lock')
 
 #If receiving error "failed to lock directory"
 #Try unlink(<directory-that-is-00LOCK>, recursive = TRUE)
@@ -29,6 +17,7 @@ library(streamMetabolizer)
 library(unitted)
 library(lubridate)
 library(tidyverse)
+library(xts)
 
 depth <- read_csv("./data/QS_depth_190404.csv")
 DO <- read_csv("./data/QS_DO_190404.csv")
@@ -62,12 +51,16 @@ qs <- depth %>%
   full_join(pres, by="datetime")
 
 qs <- qs %>% 
+  drop_na()
+
+qs <- qs %>% 
   mutate(pres_mbar = pres * 10, #convert kPa to mbar
          light = lux * 0.0185 #convert lux to ppfd
          ) 
 
 qs <- qs %>% #Use internal function to calulated DO.sat
   mutate(DO.sat = calc_DO_sat(temp=u(qs$temp, "degC"), press=u(qs$pres_mbar, "mb"), sal=u(0, "PSU")))
+
 
 qs$datetime <- qs$datetime %>% 
   force_tz(tz='Etc/GMT+5') #Assign timezone (EST, no daylight savings)
@@ -76,6 +69,9 @@ qs$solar.time <- calc_solar_time(qs$datetime, longitude = -65.8) #Calculate sola
 
 #qs now has all data
 #we are going to create new, final dataframe that mirrors example
+
+##POSSIBLE TODO:
+####Figure out how to incorporate unitted
 
 qs_dat <- qs %>% 
   select(solar.time,
@@ -86,5 +82,32 @@ qs_dat <- qs %>%
          light
          )
 
+#qs_dat <-  qs_dat %>% drop_na() 
+
 dat <- data_metab(num_days="3", res='15', attach.units = TRUE)
 
+qs_dat %>% unitted::v() %>%
+  mutate(DO.pctsat = 100 * (DO.obs / DO.sat)) %>%
+  select(solar.time, starts_with('DO')) %>%
+  gather(type, DO.value, starts_with('DO')) %>%
+  mutate(units=ifelse(type == 'DO.pctsat', 'DO\n(% sat)', 'DO\n(mg/L)')) %>%
+  ggplot(aes(x=solar.time, y=DO.value, color=type)) + geom_line() +
+  facet_grid(units ~ ., scale='free_y') + theme_bw() +
+  scale_color_discrete('variable')
+
+labels <- c(depth='depth\n(m)', temp.water='water temp\n(deg C)', light='PAR\n(umol m^-2 s^-1)')
+qs_dat %>% unitted::v() %>%
+  select(solar.time, depth, temp.water, light) %>%
+  gather(type, value, depth, temp.water, light) %>%
+  mutate(
+    type=ordered(type, levels=c('depth','temp.water','light')),
+    units=ordered(labels[type], unname(labels))) %>%
+  ggplot(aes(x=solar.time, y=value, color=type)) + geom_line() +
+  facet_grid(units ~ ., scale='free_y') + theme_bw() +
+  scale_color_discrete('variable')
+
+mm_classic <- 
+  mm_name('mle') %>% 
+  specs(day_start = 3.5, day_end = 27.5) %>% #start at 3/29 03:00 and end 4/04 03:00
+  metab(qs_dat_trunc)
+mm_classic
